aab6c0152b39a9b9257ade70801d6ee7c6ca967c
Nikola Katsarov <katsarov@gmail.com>
2026-03-18 14:56:42 +0200

    fix: apply Pint style to config/services.php

--- config/services.php
+++ config/services.php
@@ -96,9 +96,9 @@ return [
     ],
 
     'lukanet' => [
-        'client_id'     => env('LUKANET_CLIENT_ID'),
+        'client_id' => env('LUKANET_CLIENT_ID'),
         'client_secret' => env('LUKANET_CLIENT_SECRET'),
-        'redirect'      => env('LUKANET_REDIRECT_URI'),
+        'redirect' => env('LUKANET_REDIRECT_URI'),
     ],
 
     /*
